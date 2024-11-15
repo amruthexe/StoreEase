cfb86027800fa8c0a02d0409ea58199ea330a89b
amruthexe <aluriamruthrajit@gmail.com>
2024-11-15 20:53:18 +0530

    finals1122

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,8 +12,17 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // CORS configuration for production
-app.use(cors()); // This will allow all origins
+app.use(
+  cors({
+    origin: 'https://store-ease-w2i4.vercel.app', // Your frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // If your API deals with cookies or authentication
+  })
+);
 
+// Handle preflight requests
+app.options('*', cors()); // This will handle preflight requests for all routes
 
 // Routes and middleware
 app.use('/api/v1', rootRouter);
