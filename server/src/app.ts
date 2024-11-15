821cadbf50ca2b957b5ff939d9a0a6df041cf173
amruthexe <aluriamruthrajit@gmail.com>
2024-11-15 20:41:57 +0530

    finals

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,16 +7,25 @@ import globalErrorHandler from './middlewares/globalErrorhandler';
 
 const app: Application = express();
 
+// Middleware for parsing JSON and logging requests
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(cors({ origin: ['http://localhost:5173', 'https://inventory-navy.vercel.app',"https://store-ease-w2i4.vercel.app","https://store-ease-w2i4.vercel.app/login"] }));
+// CORS configuration for production
+app.use(
+  cors({
+    origin: [
+      'https://store-ease-w2i4.vercel.app', // Frontend domain in production
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true, // Allow credentials if needed
+  })
+);
 
-// application routes
+// Routes and middleware
 app.use('/api/v1', rootRouter);
-
 app.use(globalErrorHandler);
-
 app.use(notFound);
 
 export default app;
